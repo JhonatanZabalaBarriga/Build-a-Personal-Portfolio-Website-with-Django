80a45c3deb546c8616da0226f65d091063a536fc
Jhonatan Alejandro Zabala Barriga <zabalaba12@gmail.com>
2022-05-24 08:25:47 -0500

    Add gitignore file
    Add gitignore file 
    
    close #1

--- blog/models.py
+++ blog/models.py
@@ -10,8 +10,14 @@ class Post(models.Model):
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField('Category', related_name='posts')
 
+    def __unicode__(self):
+        return self.title
+
 class Comment(models.Model):
     author = models.CharField(max_length=60)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     post = models.ForeignKey('Post', on_delete=models.CASCADE)
+
+    def __unicode__(self):
+        return self.title
